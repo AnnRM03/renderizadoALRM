e5ba96703d56eaeb327200a0dd0946b2071f8061
AnnRM03 <127805214+AnnRM03@users.noreply.github.com>
2024-09-19 21:35:22 -0600

    Add files via upload

---
+++ srv/datos.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__ . "/../lib/php/devuelveJson.php";
+
+devuelveJson([
+    "pop" => "Michael Jackson Michael Jackson es conocido como el Rey del Pop 
+    y es uno de los artistas más influyentes en la historia de la música. 
+    Comenzó su carrera como parte de The Jackson 5 y más tarde se convirtió en un 
+    fenómeno mundial como solista. 
+    Sus álbumes, como Thriller (el álbum más vendido de todos los tiempos), 
+    Bad y Off the Wall, redefinieron el pop y la música en general.
+    Éxitos populares: Billie Jean, Thriller, Beat It, Smooth Criminal.",
+
+    "rock" => "Arctic Monkeys es una banda británica de rock formada en Sheffield
+    en 2002. Son conocidos por haber sido una de las primeras bandas en ganar popularidad 
+    a través de internet y su debut, Whatever People Say I Am, That's What I'm Not, es uno de 
+    los álbumes más aclamados de la música británica.
+    Éxitos populares: Do I Wanna Know?, I Bet You Look Good on the Dancefloor, R U Mine?.",
+
+    "mi" => "Chase Atlantic es una banda australiana que mezcla géneros 
+    como el R&B, rock alternativo, trap y pop. Han ganado notoriedad por su estilo 
+    experimental y oscuro. La banda se formó en 2014 y ha lanzado varios EPs y álbumes 
+    con un enfoque lírico introspectivo y atmosférico.
+    Éxitos populares: Friends, Swim, Into It.",
+
+    "rb" => "Abel Tesfaye, conocido como The Weeknd, es un cantante, compositor y productor 
+    canadiense que ha revolucionado el R&B moderno con su mezcla de sonidos electrónicos, 
+    pop y dark wave. Su ascenso a la fama fue impulsado por su misterio inicial y sus letras 
+    melancólicas y oscuras. Ha ganado múltiples premios y es considerado uno de los artistas más 
+    influyentes de la década.
+    Éxitos populares: Blinding Lights, Can't Feel My Face, Starboy, The Hills.",
+
+    "banda" => "Julión Álvarez es un cantante mexicano de música regional, especializado en banda 
+    y norteño. Se dio a conocer como líder de la banda Julión Álvarez y su Norteño Banda y es uno 
+    de los artistas más populares en el género de la música de banda en México.
+    Éxitos populares: Y Así Fue, El Amor de Su Vida, Te Hubieras Ido Antes."
+]);
+?>
---
+++ srv/procesa.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once __DIR__ . "/../lib/php/recuperaTexto.php";
+require_once __DIR__ . "/../lib/php/devuelveJson.php";
+
+$genero = recuperaTexto("genero");
+
+$artistaRecomendado = "";
+
+switch ($genero) {
+    case "pop":
+        $artistaRecomendado = "Michael Jackson";
+        break;
+    case "rock":
+        $artistaRecomendado = "Arctic Monkeys";
+        break;
+    case "mi":
+        $artistaRecomendado = "Chase Atlantic";
+        break;
+    case "rb":
+        $artistaRecomendado = "The Weeknd";
+        break;
+    case "banda":
+        $artistaRecomendado = "Julión Álvarez";
+        break;
+}
+
+if($artistaRecomendado==""){
+    $resultado = "Por favor selecciona un género.";
+}else{
+    $resultado = "Te recomiendo escuchar a: {$artistaRecomendado}.";
+}
+devuelveJson($resultado);
+?>
